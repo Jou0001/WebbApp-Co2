40b619dad6167a8ff501b44402fcc0cdfb381ed2
Jou0001 <jout0001@edu.zealand.dk>
2024-12-02 12:15:25 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const URI = "";
+Vue.createApp({
+    data() {
+        return {
+            username: '',
+            password: '',
+
+            
+        };
+    },
+    methods: {
+        async Login() {
+            const response = await axios.post(uri, {
+                username: this.username,
+                password: this.password
+            });
+            if (response.status != 200) 
+                return
+            
+            
+        },
+       
+    }
+}).mount("#app");

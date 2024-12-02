a5503891ef11b77bd13611383236a5bbc0ffed05
Jeppe August Hirsch Jeppsson <jeppe.jeppsson@live.dk>
2024-12-02 14:19:56 +0100

    login visual change

--- index.js
+++ index.js
@@ -24,16 +24,16 @@ Vue.createApp({
     },
     methods: {
         async Login() {
-            console.log("Trying to login as: " + this.username + " <-> " + this.password)
             const response = await axios.post(URI + "/login", {
                 username: this.username,
                 password: this.password
             }, this.validationStatus);
             if (response.status != 200)
             {
-                errorMessage = "Your username or password was incorrect"
+                this.errorMessage = "Your username or password was incorrect"
                 return
-            }            
+            }
+            this.errorMessage = null
             this.userId = response.data
             console.log(this.userId)
             

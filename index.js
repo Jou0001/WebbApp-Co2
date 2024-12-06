b82291aed1e20de876d1f335ee37213826776523
Jeppe Jeppsson <jeppe.jeppsson@live.dk>
2024-12-06 14:36:30 +0100

    Trying to fix chart, so it only uses some of the data it gets from the rest api

--- index.js
+++ index.js
@@ -76,30 +76,35 @@ Vue.createApp({
             const d = new Date()
             var os = d.getTimezoneOffset();
             const today = new Date(d.getFullYear(), d.getMonth(), d.getDate() + 1)
+            var measurementsPerDay = []
             var startDay
             // get data
             switch (this.chartShowDataMode)
             {
-                case 1: // week
+                case 1: // week // 2 per day
                 currentDate = new Date()
                 currentDate.setUTCDate(currentDate.getUTCDate() - 7)
                 startDay = { Day: currentDate.getDate(), Month: currentDate.getMonth(), Year: currentDate.getFullYear() }
+                measurementsPerDay = [8, 16]
                 break
-                case 2: // month
+                case 2: // month // 1 per day
                 startDay = { Day: d.getDate(), Month: (d.getMonth() - 1), Year: d.getFullYear() }
                 if (startDay.Month < 0)
                 {
                     startDay.Month = 11
                     startDay.Year -= 1
                 }
+                measurementsPerDay = [12]
                 break
-                case 3: // all time
+                case 3: // all time // 1 per day
                 //startDay = { Day: 1, Month: 1, Year: 1999 }
                 startDay = null
+                measurementsPerDay = [12] // works wierd if all time is too long, proboably
                 break
-                case 0: // day
+                case 0: // day // 1 per hour
                 default:
                 startDay = { Day: d.getDate(), Month: d.getMonth(), Year: d.getFullYear() }
+                measurementsPerDay = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
                 break
             }
             if (startDay)
@@ -124,14 +129,53 @@ Vue.createApp({
                 console.log("No data in timeframe")
                 return
             }
-            console.log(response)
+            // filter data quantity
+            var usedData = []
+            var lastMeasurementTime = new Date(response.data[0].measurementTime)
+            var measurementsPerDayIndex = 0
+            var lastDate = -1
+            var date = lastMeasurementTime.getDate()
+            //usedData.push(response.data[0])
+            // measurementsPerDay
+            for (var i = 1; i < response.data.length; i++)
+            {
+                var currentMeasurementTime = new Date(response.data[i].measurementTime)
+                if (currentMeasurementTime.getDate() == lastDate)
+                    continue
+                var previousIterationMeasurementTime = new Date(response.data[i - 1].measurementTime)
+                // get distance to measurementsPerDay[measurementsPerDayIndex], and compare with last iteration
+                var targetTime = measurementsPerDay[measurementsPerDayIndex] * 60 * 60 + date * 60 * 60 * 24
+                var currentTime = currentMeasurementTime.getHours() * 60 * 60 + currentMeasurementTime.getMinutes() * 60 + currentMeasurementTime.getSeconds() + currentMeasurementTime.getDate() * 60 * 60 * 24
+                var oldTime = previousIterationMeasurementTime.getHours() * 60 * 60 + previousIterationMeasurementTime.getMinutes() * 60 + previousIterationMeasurementTime.getSeconds() + previousIterationMeasurementTime.getDate() * 60 * 60 * 24
+                console.log("target " + i + ": " + targetTime + " <-> " + currentTime + " : " + oldTime)
+                var distCurrentTime = Math.abs(targetTime - currentTime)
+                var distOldTime = Math.abs(targetTime - oldTime)
+                console.log("timeDiff " + i + ": " + distCurrentTime + " <-> " + distOldTime)
+                if (distCurrentTime > distOldTime)
+                    continue
+                // add the time :)
+                lastMeasurementTime = previousIterationMeasurementTime
+                usedData.push(response.data[i - 1])
+                measurementsPerDayIndex++
+                if (measurementsPerDayIndex == measurementsPerDay.length || lastMeasurementTime.getDate() != date)
+                {
+                    measurementsPerDayIndex = 0
+                    currentDate = new Date(response.data[i].measurementTime)
+                    currentDate.setUTCDate(currentDate.getUTCDate() + 1)
+                    date = currentDate.getDate()
+                }
+                
+            }
+            // we always want the newest data along with some of the old data
+            usedData.push(response.data[response.data.length - 1])
+            
             var labels = []
             var data = []
-            for (var i = 0; i < response.data.length; i++)
+            for (var i = 0; i < usedData.length; i++)
             {
-                console.log(response.data[i])
-                labels.push(response.data[i].measurementTime)
-                data.push(response.data[i].measurementValue)
+                console.log(usedData[i])
+                labels.push(usedData[i].measurementTime)
+                data.push(usedData[i].measurementValue)
             }
             console.log(labels)
             console.log(data)
@@ -144,7 +188,7 @@ Vue.createApp({
             //this.CO2ChartSource = "https://quickchart.io/chart?width=500&height=300&chart={type:'line',data:{labels:['January','February', 'March','April', 'May'], datasets:[{label:'Dogs',data:[50,60,70,180,190], fill: false},{label:'Cats',data:[100,200,300,400,500], fill: false}]}}"
             this.CO2ChartSource = "https://quickchart.io/chart?width=500&height=300&chart={type:'line',data:{labels:" + labelsAsArrayString + ", datasets:[{label:'CO2 measurement',data:["+ data+"], fill: false}]}}"
             // https://quickchart.io/chart?width=500&height=300&chart={type:'line',data:{labels:['2024-12-06T10:30:15.457'], datasets:[{label:'CO2 measurement',data:[1000], fill: false}]}}
-            SetChartImage()
+            this.SetChartImage()
         },
 
         SetChartImage() {

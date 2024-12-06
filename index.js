e7b8442a815f9f9b4147f319e79e73984d422861
Jeppe Jeppsson <jeppe.jeppsson@live.dk>
2024-12-06 12:05:02 +0100

    Chart works, needs improvement

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ Vue.createApp({
             userId: null,
             errorMessage: null,
             CO2ChartSource: "",
+            chartShowDataMode: 0, // 0 = day, 1 = week, 2 = month, 3 = all time
             warningValue: 1000,  
             showInput: false,  
             activeSensor: null,
@@ -63,9 +64,87 @@ Vue.createApp({
             console.log(this.userId);
             document.location.href = "index.html";
         },
-
-        async SetChartData() {
-            this.CO2ChartSource = "https://quickchart.io/chart?width=500&height=300&chart={type:'line',data:{labels:['January','February', 'March','April', 'May'], datasets:[{label:'Dogs',data:[50,60,70,180,190], fill: false},{label:'Cats',data:[100,200,300,400,500], fill: false}]}}";
+        async SetChartDataWithValue(value)
+        {
+            this.chartShowDataMode = value
+            this.SetChartData()
+        },
+        async SetChartData()
+        {
+            this.CO2ChartSource = null
+            // get date
+            const d = new Date()
+            var os = d.getTimezoneOffset();
+            const today = new Date(d.getFullYear(), d.getMonth(), d.getDate() + 1)
+            var startDay
+            // get data
+            switch (this.chartShowDataMode)
+            {
+                case 1: // week
+                currentDate = new Date()
+                currentDate.setUTCDate(currentDate.getUTCDate() - 7)
+                startDay = { Day: currentDate.getDate(), Month: currentDate.getMonth(), Year: currentDate.getFullYear() }
+                break
+                case 2: // month
+                startDay = { Day: d.getDate(), Month: (d.getMonth() - 1), Year: d.getFullYear() }
+                if (startDay.Month < 0)
+                {
+                    startDay.Month = 11
+                    startDay.Year -= 1
+                }
+                break
+                case 3: // all time
+                //startDay = { Day: 1, Month: 1, Year: 1999 }
+                startDay = null
+                break
+                case 0: // day
+                default:
+                startDay = { Day: d.getDate(), Month: d.getMonth(), Year: d.getFullYear() }
+                break
+            }
+            if (startDay)
+                startDay = new Date(startDay.Year, startDay.Month, startDay.Day)
+            // get our data from rest api
+            this.activeSensor = 1
+            var quiries
+            if (startDay)
+                var quiries = "?startTime=" + new Date(startDay.getTime() - os * 60 * 1000).toJSON()+"&endTime=" + new Date(today.getTime() - os * 60 * 1000).toJSON()
+            else
+                var quiries = "" // all time, don't need any quiries :)
+            // testing via swagger we notices all ":" were replaced with "%3A", in the quiries, at least for DateTime
+            var response = await axios.get(URICO2 + "/" + this.activeSensor + quiries.replaceAll(":", "%3A"), this.validateStatus)
+            
+            if (response.status != 200)
+            {
+                console.log("Unable to retrieve CO2 data: " + response.status)
+                return
+            }
+            if (response.data.length == 0)
+            {
+                console.log("No data in timeframe")
+                return
+            }
+            console.log(response)
+            var labels = []
+            var data = []
+            for (var i = 0; i < response.data.length; i++)
+            {
+                console.log(response.data[i])
+                labels.push(response.data[i].measurementTime)
+                data.push(response.data[i].measurementValue)
+            }
+            console.log(labels)
+            console.log(data)
+            var labelsAsArrayString = "["
+            for (var i = 0; i < labels.length; i++)
+                labelsAsArrayString += "'" + labels[i] + "',"
+            labelsAsArrayString = labelsAsArrayString.slice(0, labelsAsArrayString.length - 1)
+            labelsAsArrayString += "]"
+            // get chart from external rest api
+            //this.CO2ChartSource = "https://quickchart.io/chart?width=500&height=300&chart={type:'line',data:{labels:['January','February', 'March','April', 'May'], datasets:[{label:'Dogs',data:[50,60,70,180,190], fill: false},{label:'Cats',data:[100,200,300,400,500], fill: false}]}}"
+            this.CO2ChartSource = "https://quickchart.io/chart?width=500&height=300&chart={type:'line',data:{labels:" + labelsAsArrayString + ", datasets:[{label:'CO2 measurement',data:["+ data+"], fill: false}]}}"
+            // https://quickchart.io/chart?width=500&height=300&chart={type:'line',data:{labels:['2024-12-06T10:30:15.457'], datasets:[{label:'CO2 measurement',data:[1000], fill: false}]}}
+            SetChartImage()
         },
 
         SetChartImage() {
